d6bfd42599dbac2ab5a1c9909706c6fe1d6fc197
Gorg Onischenko <gonische@c1r15s1.42wolfsburg.de>
2024-06-14 22:35:37 +0200

    Add tests

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: gonische <gonische@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/12 11:33:20 by gonische          #+#    #+#             */
-/*   Updated: 2024/06/12 11:58:23 by gonische         ###   ########.fr       */
+/*   Created: 2024/06/14 22:12:02 by gonische          #+#    #+#             */
+/*   Updated: 2024/06/14 22:35:02 by gonische         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,17 +32,42 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	return (dstlen + srclen);
 }
 
-/*
 // Tests
+/*
+	For tests compile this programm with cc -lbsd ft_strlcat.c ft_memcpy.c ft_strlen.c
+*/
+/*
+#include <bsd/string.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+void	test(char *dest, char *src, size_t dest_len)
+{
+	char *ft_strlcat_dest = (char *)malloc(dest_len + 1);
+	ft_strlcat_dest[dest_len] = '\0';
+
+	char *strlcat_dest = (char *)malloc(dest_len + 1);
+	strlcat_dest[dest_len] = '\0';
+
+	ft_memcpy(ft_strlcat_dest, dest, dest_len);
+	ft_memcpy(strlcat_dest, dest, dest_len);
+
+	size_t	ft_strlcat_result = ft_strlcat(ft_strlcat_dest, src, dest_len);
+	size_t	strlcat_result = strlcat(strlcat_dest, src, dest_len);
+	if (ft_strlcat_result == strlcat_result)
+		printf("PASS!\n");
+	else
+		printf("FAILED: ft_strlcat: %s [%zu], strlcat: %s [%zu]\n", 
+			   ft_strlcat_dest, ft_strlcat_result, strlcat_dest, strlcat_result);
+}
+
 int	main(void)
 {
-	char	*src = " World!";
-	size_t	dest_len = 15;
-	char	dest[15] = "Hello";
-	size_t result = ft_strlcat(&dest[0], src, dest_len);
-	printf("Result: %d\n\
-			Created string: %s\n", (int)result, dest);
+	test("Hello", " World!", 14);
+	test("", " World!", 14);
+	test("World!", "", 0);
+	test("Hello", " World!", 0);
+	test("Hello", " World!", 1);
 	return (0);
 }
 */
