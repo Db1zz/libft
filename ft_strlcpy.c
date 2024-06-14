20e424c556a37dff4ff9d59a120e672f3e4b16ac
Gorg Onischenko <gonische@c1r15s1.42wolfsburg.de>
2024-06-14 22:50:34 +0200

    Clean files, add tests for ft_strlcpy.c

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: gonische <gonische@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 20:20:50 by gonische          #+#    #+#             */
-/*   Updated: 2024/06/12 12:38:38 by gonische         ###   ########.fr       */
+/*   Updated: 2024/06/14 22:49:34 by gonische         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,17 +31,42 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	return (result);
 }
 
-/*
 // Tests
+/*
+	For tests compile this programm with cc -lbsd ft_strlcpy.c ft_strlen.c
+*/
+/*
 #include <stdio.h>
+#include <stdlib.h>
+#include <bsd/string.h>
+
+void	test(size_t dest_len, const char *src)
+{
+	char *ft_strlcpy_dest = (char *)malloc(dest_len + 1);
+	ft_strlcpy_dest[dest_len] = '\0';
+
+	char *strlcpy_dest = (char *)malloc(dest_len + 1);
+	strlcpy_dest[dest_len] = '\0';
+
+	size_t ft_strlcpy_res = ft_strlcpy(ft_strlcpy_dest, src, dest_len);
+	size_t ftrlcpy_res = strlcpy(strlcpy_dest, src, dest_len);
+	if (ft_strlcpy_res == ftrlcpy_res)
+		printf("PASS!\n");
+	else
+		printf("FAILED: ft_strlcat: %s [%zu], strlcat: %s [%zu]\n", 
+			   ft_strlcpy_dest, ft_strlcpy_res, strlcpy_dest, ftrlcpy_res);
+	free(ft_strlcpy_dest);
+	free(strlcpy_dest);
+}
+
 
 int	main(void)
 {
-	size_t	len;
-	char	dst[5];
-
-	len = ft_strlcpy(&dst[0], "Hello World!\n", 14);
-	printf("%s\n", dst);
+	test(10, "Hello World!\n");
+	test(0, "Hello World!\n");
+	test(10, "");
+	test(0, "");
+	test(0, "");
 	return (0);
 }
 */
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gonische <gonische@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 14:30:25 by gonische          #+#    #+#             */
-/*   Updated: 2024/06/14 16:59:11 by gonische         ###   ########.fr       */
+/*   Updated: 2024/06/14 22:49:52 by gonische         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,8 +36,8 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_atoi(const char *str);
-void	*ft_calloc(size_t count, size_t size); // Write Tests
-char	*ft_strdup(char *src);				   // Write Tests
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_strdup(char *src);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
